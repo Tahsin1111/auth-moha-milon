44c853d0df79f71709db17d3c379f34c711a4160
Tahsin1111 <entimojumder@gmail.com>
2023-10-06 22:53:15 +0600

    Create User Context for shared authentication

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import Root from './components/Root';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
+import AuthProvider from './providers/AuthProvider';
 
 
 
@@ -21,8 +22,8 @@ const router = createBrowserRouter([
     element: <Root></Root>,
     children: [
       {
-        path :'/',
-        element : <Home></Home>
+        path: '/',
+        element: <Home></Home>
       },
       {
         path: '/login',
@@ -30,7 +31,7 @@ const router = createBrowserRouter([
       },
       {
         path: '/register',
-        element:<Register></Register>
+        element: <Register></Register>
       }
 
     ]
@@ -39,6 +40,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
